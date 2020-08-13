ce65994a36dc8359818f2bb2cbf32980344821fe
RAJ MEHTA <rajmehta28599v@gmail.com>
2020-08-13 20:16:32 +0530

    Add files via upload

---
+++ master_30-7.ino
@@ -0,0 +1,30 @@
+#include <Wire.h>
+
+int x = 0;
+int y = 0;
+
+void setup() {
+  // Start the I2C Bus as Master
+  Wire.begin(3);
+  Wire.onReceive(receiveEvent);
+  Serial.begin(115200);
+}
+
+void receiveEvent(int bytes) {
+  x = Wire.read();    // read one character from the I2C
+  y = Wire.read();
+}
+
+void loop() {
+  Serial.print("x= ");
+  Serial.print(x);
+  Serial.print("y= ");
+  Serial.println(y);
+
+  Wire.beginTransmission(9); // transmit to device #9
+  Wire.write(19);              // sends x
+  Wire.endTransmission();    // stop transmitting
+
+
+}
+
---
+++ slave_30-7.ino
@@ -0,0 +1,30 @@
+#include <Wire.h>
+int x = 0;
+int y = 0,f=0;
+
+void setup() {
+  // Start the I2C Bus as Slave on address 9
+  Wire.begin(9);
+  // Attach a function to trigger when something is received.
+  Wire.onReceive(receiveEvent);
+  Serial.begin(115200);
+}
+
+void receiveEvent(int bytes) {
+  x = Wire.read();    // read one character from the I2C
+}
+
+void loop() {
+  Serial.println(x);
+  y++;
+  if (x == 19)
+  {
+    Wire.beginTransmission(3);
+    Wire.write(y);
+    Wire.write(50);
+    Wire.endTransmission();
+  }
+
+
+}
+
